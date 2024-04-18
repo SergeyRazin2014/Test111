f0818de1af2c3f08885df7da5a00a900697934d0
Sergei Razin <900556@mail.ru>
2024-04-18 18:01:09 +0700

    lesson 14, 15

---
+++ consult_14/index.js
@@ -0,0 +1,12 @@
+const myDiv = document.getElementById('myDiv');
+console.log(myDiv);
+
+const spaceshep = {
+    name: 'space',
+}
+
+const test = 'hello world';
+myDiv.innerText = spaceshep.name;
+
+
+
